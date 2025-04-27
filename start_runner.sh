402b5956239662e61288de1fb3acdb512b9d9958
magowiz <magowiz@gmail.com>
2025-04-27 03:26:07 +0200

    improve runner docker

--- start_runner.sh
+++ start_runner.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker rm -f runner_devildex 2>/dev/null || true
 docker build -f runner/Dockerfile . -t runner_devildex
-docker run -d --name runner_devildex runner_devildex
+docker run -d -it \
+  --env="DISPLAY" \
+  --env="XAUTHORITY=/root/.Xauthority" \
+  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --name runner_devildex runner_devildex
+
