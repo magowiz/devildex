175e6cdbd510b50547a8f14bdc1bab1f6a5936cb
magowiz <magowiz@gmail.com>
2025-05-07 01:25:24 +0200

    lint shell scripts

--- start_runner.sh
+++ start_runner.sh
@@ -2,9 +2,8 @@
 docker rm -f runner_devildex 2>/dev/null || true
 docker build -f runner/Dockerfile . -t runner_devildex
 docker run -d -it \
-  --env="DISPLAY" \
-  --env="XAUTHORITY=/root/.Xauthority" \
-  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --name runner_devildex runner_devildex
-
+	--env="DISPLAY" \
+	--env="XAUTHORITY=/root/.Xauthority" \
+	--volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--name runner_devildex runner_devildex
