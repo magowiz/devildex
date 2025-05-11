20bf00fe33a10c0ab484d548d03a936115003121
magowiz <magowiz@gmail.com>
2025-05-11 22:22:04 +0200

    update sphinx configuration

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 #
+import os
+import sys
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -13,11 +15,45 @@ author = 'magowiz'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 templates_path = ['_templates']
 exclude_patterns = []
-
+html_theme_options = {
+    # Set the name of the project to appear in the navigation.
+    "nav_title": "DevilDex",
+    # Set you GA account ID to enable tracking
+    # 'google_analytics_account': 'UA-XXXXX',
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    # 'base_url': 'https://project.github.io/project',
+    # Set the color and the accent color
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    # Set the repo location to get a badge with stats
+    # 'repo_url': 'https://github.com/project/project/',
+    # 'repo_name': 'Project',
+    # Visible levels of the global TOC; -1 means unlimited
+    "globaltoc_depth": 3,
+    # If False, expand all TOC entries
+    "globaltoc_collapse": False,
+    # If True, show hidden TOC entries
+    "globaltoc_includehidden": False,
+}
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -25,3 +61,5 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+sys.path.insert(0, os.path.dirname(os.path.abspath(".")))
+print(os.path.dirname(os.path.abspath("..")))
