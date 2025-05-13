3a78873698175871741262a731aa895b1d5f474c
magowiz <magowiz@gmail.com>
2025-05-14 00:08:49 +0200

    lint code

--- src/devildex/docstrings/docstrings_src.py
+++ src/devildex/docstrings/docstrings_src.py
@@ -17,9 +17,7 @@ import pdoc
 from devildex import info
 from devildex.utils.venv_cm import IsolatedVenvManager
 from devildex.utils.venv_utils import (
-    execute_command,
-    install_project_and_dependencies_in_venv,
-)
+    execute_command, install_project_and_dependencies_in_venv)
 
 logger = logging.getLogger(__name__)
 CONFIG_FILE = "../../../devildex_config.ini"
--- src/devildex/readthedocs/readthedocs_src.py
+++ src/devildex/readthedocs/readthedocs_src.py
@@ -7,17 +7,16 @@ import os
 import re
 import shutil
 import subprocess
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Any, Callable
+
 import requests
 
 from devildex.info import PROJECT_ROOT, VERSION
 from devildex.utils.venv_cm import IsolatedVenvManager
 from devildex.utils.venv_utils import (
-    execute_command,
-    install_project_and_dependencies_in_venv,
-)
+    execute_command, install_project_and_dependencies_in_venv)
 
 logger = logging.getLogger(__name__)
 if not logger.hasHandlers():
