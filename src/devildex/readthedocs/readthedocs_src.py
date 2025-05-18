157d9c71234898c51c1fcf964f387a9dd3e62b38
magowiz <magowiz@gmail.com>
2025-05-18 23:56:12 +0200

    lint file

--- src/devildex/readthedocs/readthedocs_src.py
+++ src/devildex/readthedocs/readthedocs_src.py
@@ -12,11 +12,12 @@ from pathlib import Path
 from typing import Any, Callable
 
 import requests
-
 from devildex.info import PROJECT_ROOT, VERSION
 from devildex.utils.venv_cm import IsolatedVenvManager
 from devildex.utils.venv_utils import (
-    execute_command, install_project_and_dependencies_in_venv)
+    execute_command,
+    install_project_and_dependencies_in_venv,
+)
 
 logger = logging.getLogger(__name__)
 if not logger.hasHandlers():
