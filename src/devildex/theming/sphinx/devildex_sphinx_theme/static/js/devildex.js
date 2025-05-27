58398f910a40b7f0e0289b26ae93c87d7a128e9d
magowiz <magowiz@gmail.com>
2025-05-27 13:56:27 +0200

    some theme improvements

--- src/devildex/theming/sphinx/devildex_sphinx_theme/static/js/devildex.js
+++
@@ -1,8 +0,0 @@
-            document.addEventListener('DOMContentLoaded', function() {
-              if (!document.getElementById('devildex-sphinx-banner')) {
-                const banner = document.createElement('div');
-                banner.id = 'devildex-sphinx-banner';
-                banner.textContent = 'DEVILDEX SPHINX THEME';
-                document.body.insertBefore(banner, document.body.firstChild);
-              }
-            });
