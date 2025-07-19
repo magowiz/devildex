02d0a2e979b2c6982cb2b946a8c26c3cb9031484
magowiz <magowiz@gmail.com>
2025-07-19 19:36:50 +0200

    linted install_pipx.sh

--- install_pipx.sh
+++ install_pipx.sh
@@ -5,14 +5,14 @@ export PIP_FIND_LINKS="${WXPYTHON_EXTRAS_URL}"
 DEVILDEX_WHEEL="./dist/devildex-0.1.0-py3-none-any.whl"
 
 if [ ! -f "$DEVILDEX_WHEEL" ]; then
-    echo "Error: devildex wheel file not found: $DEVILDEX_WHEEL"
-    exit 1
+	echo "Error: devildex wheel file not found: $DEVILDEX_WHEEL"
+	exit 1
 fi
 
 echo "Installing devildex, with wxPython preinstalled from URL extras..."
 pipx install \
-    --preinstall "wxPython" \
-    --pip-args="--find-links ${WXPYTHON_EXTRAS_URL}" \
-    "$DEVILDEX_WHEEL"
+	--preinstall "wxPython" \
+	--pip-args="--find-links ${WXPYTHON_EXTRAS_URL}" \
+	"$DEVILDEX_WHEEL"
 
 echo "Installation completed."
